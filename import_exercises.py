49af40f50a74eb994ff073f343337513498b2675
Paige Rackley <paige.e.rackley@gmail.com>
2022-04-14 14:28:58 -0500

    Finished Import Exercises

---
+++ import_exercises.py
@@ -0,0 +1,8 @@
+# b. Within this file, use from to import the calculate_tip function directly. 
+# Call this function with values you choose and print the result.
+from functions_exercises import calculate_tip
+calculate_tip(45, .20)
+
+import itertools
+# 2a. How many different ways can you combine the let
+list(itertools.product('acbc', '123'))
