642c6ee3fcdddfa71569dc4fbf253606e5b071f6
Paige Rackley <paige.e.rackley@gmail.com>
2022-04-14 16:27:54 -0500

    Done, bonus will be on diff doc

--- import_exercises.py
+++ import_exercises.py
@@ -6,3 +6,7 @@ calculate_tip(45, .20)
 import itertools
 # 2a. How many different ways can you combine the let
 list(itertools.product('acbc', '123'))
+# 2b. How many different combinations are there of 2 letters from "abcd"?
+list(itertools.combinations('abcd', 2))
+# 2c. How many different permutations are there of 2 letters from "abcd"?
+list(itertools.permutations('abcd', 2))
